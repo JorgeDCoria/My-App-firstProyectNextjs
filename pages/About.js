fa5037c4ca1985e8f4df51c65720a0a9228c1531
Coria Jorge <coriajorge.dev.apu@gmail.com>
2022-12-09 12:41:58 -0300

    feat/create proyect with Nextjs and added tailwind

---
+++ pages/About.js
@@ -0,0 +1,9 @@
+import PageLayaout from "./components/PageLayout";
+
+export default function About() {
+  return (
+    <PageLayaout title="About">
+      <h1>Esto es un about</h1>
+    </PageLayaout>
+  );
+}
---
+++ pages/article/[id].js
@@ -0,0 +1,6 @@
+export default function Article (){
+  return(
+   <h1>
+    esto es un articulo </h1>
+  );
+}
